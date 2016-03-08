aab2cc067a09e1416124c464eaa506014974053f
Bendik Solheim <bendik@solheim.im>
2016-03-08 22:01:54 +0100

    Refactor to use reader monad and ScottyT

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,48 +1,84 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE FlexibleContexts           #-}
 
 module Lib where
 
-import           Control.Monad.IO.Class      (liftIO)
-import           Control.Monad.Logger        (runStderrLoggingT)
-import           Database.Persist.Postgresql (ConnectionString,
-                                              withPostgresqlPool)
-import           Database.Persist.Sql        (ConnectionPool)
+import           Control.Monad.IO.Class               (MonadIO, liftIO)
+import           Control.Monad.Trans.Reader           (ReaderT, runReaderT)
+import           Control.Monad.Reader.Class           (MonadReader)
+import           Control.Monad.Trans.Class            (MonadTrans, lift)
+import           Control.Monad.Reader                 (asks)
+import           Control.Monad.Logger                 (runStdoutLoggingT)
+import qualified Data.Text.Lazy                       as T
+import           Database.Persist.Postgresql as DB
+
+import qualified Web.Scotty()
+import           Web.Scotty.Trans as S
+
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+
 import           Models.Models
-import           Web.Scotty
+
+data Config = Config { getPool :: DB.ConnectionPool }
+
+newtype ConfigM a = ConfigM
+  { runConfigM :: ReaderT Config IO a
+  } deriving (Applicative, Functor, Monad,
+              MonadIO, MonadReader Config)
+
+type Error = T.Text
+type Action = ActionT Error ConfigM ()
 
 connStr :: ConnectionString
 connStr = "host=localhost dbname=listify user=test password=test port=5432"
 
-getIndexH :: ActionM ()
+getIndexH :: Action
 getIndexH = html "Hello, world"
 
-getItemsH :: ConnectionPool -> ActionM ()
-getItemsH pool = do
-  items <- liftIO $ listItems pool
+getItemsH :: Action
+getItemsH = do
+  items :: [Entity Item] <- runDb (DB.selectList [] [])
   json items
 
-getItemH :: ConnectionPool -> ActionM ()
-getItemH pool = do
+intToKey :: ToBackendKey SqlBackend a => Integer -> Key a
+intToKey intId = toSqlKey $ fromIntegral intId
+
+getItemH :: Action
+getItemH = do
   itemId <- param "id"
-  item   <- liftIO $ getItem pool itemId
+  item :: Maybe Item <- runDb (DB.get (toSqlKey itemId))
   json item
 
-createItemH :: ConnectionPool -> ActionM ()
-createItemH pool = do
-  item         <- jsonData
-  insertedItem <- liftIO $ insertItem pool item
+createItemH :: Action
+createItemH = do
+  item :: Item <- jsonData
+  itemId <- runDb $ insert item
+  insertedItem <- runDb $ DB.get itemId
   json insertedItem
 
-routes :: ConnectionPool -> ScottyM ()
-routes pool = do
-  get  "/" getIndexH
-  get  "/items" (getItemsH pool)
-  post "/items" (createItemH pool)
-  get  "/items/:id" (getItemH pool)
+runDb :: (MonadTrans t, MonadIO (t ConfigM)) => DB.SqlPersistT IO a -> t ConfigM a
+-- runDb :: (MonadIO m, MonadReader Config m) => SqlPersistT IO b -> m b
+runDb query = do
+  pool <- lift $ asks getPool
+  liftIO $ runSqlPool query pool
+
+routes :: ScottyT Error ConfigM ()
+routes = do
+  middleware logStdoutDev
+  S.get "/" getIndexH
+  S.get "/items" getItemsH
+  S.post "/items" createItemH
+  S.get  "/items/:id" getItemH
+
+doMigration :: ReaderT SqlBackend IO ()
+doMigration = DB.runMigration migrateAll
 
 runApplication :: IO ()
-runApplication =
-  runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool ->
-  liftIO $ initialize pool >> scotty 3000 (routes pool)
+runApplication = do
+  pool <- runStdoutLoggingT $ createPostgresqlPool connStr 10
+  let cfg = Config pool
+  let r m = runReaderT (runConfigM m) cfg
+  runSqlPool doMigration pool
+  scottyT 3000 r routes
--- src/Models/Models.hs
+++ src/Models/Models.hs
@@ -12,7 +12,7 @@
 
 module Models.Models where
 
-import           Database.Persist.Sql
+-- import           Database.Persist.Sql
 import           Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
@@ -21,20 +21,3 @@ Item json
     count Int default=1
     deriving Show
 |]
-
-intToKey :: ToBackendKey SqlBackend a => Integer -> Key a
-intToKey intId = toSqlKey $ fromIntegral intId
-
-initialize :: ConnectionPool -> IO ()
-initialize pool = flip runSqlPool pool $ runMigration migrateAll
-
-listItems :: ConnectionPool -> IO [Entity Item]
-listItems = runSqlPool (selectList ([] :: [Filter Item]) [])
-
-insertItem :: ConnectionPool -> Item -> IO (Maybe Item)
-insertItem pool item = do
-  itemId <- runSqlPool (insert item) pool
-  runSqlPool (get itemId) pool
-
-getItem :: ConnectionPool -> Integer -> IO (Maybe Item)
-getItem pool intId = flip runSqlPool pool . get . intToKey $ intId
